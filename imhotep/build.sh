130e14feb2b73e8cea050799361c0032f1e660e8
darren <darren@indeed.com>
2014-10-27 14:29:15 -0500

    Remove maven installation - no longer necessary

--- imhotep/build.sh
+++ imhotep/build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash -v 
 
 # install maven
-cd /var/data/imhotep
-mkdir build-tools
-cd build-tools
-wget http://apache.cs.utah.edu/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz
-tar xf apache-maven-3.2.1-bin.tar.gz
-ln -s apache-maven-3.2.1 maven
+# cd /var/data/imhotep
+# mkdir build-tools
+# cd build-tools
+# wget http://apache.cs.utah.edu/maven/maven-3/3.2.1/binaries/apache-maven-3.2.1-bin.tar.gz
+# tar xf apache-maven-3.2.1-bin.tar.gz
+# ln -s apache-maven-3.2.1 maven
 
 # download and build imhotep
-#cd /var/data/imhotep
-#git clone https://github.com/darren-indeed/imhotep.git source
-#cd source
-#/var/data/imhotep/build-tools/maven/bin/mvn install package
+# cd /var/data/imhotep
+# git clone https://github.com/darren-indeed/imhotep.git source
+# cd source
+# /var/data/imhotep/build-tools/maven/bin/mvn install package
 
 # copy imhotep since building it does not work yet
 mkdir -p /var/data/imhotep/source/target
